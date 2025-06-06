4a8894d637b05b65207800d878b2830c1b0b5bb5
Jorge Devops <bk11iscool@gmail.com>
2025-06-05 20:01:36 -0700

    Solution to re-regroup-groups problem

---
+++ re-group-groups-problem.py
@@ -0,0 +1,26 @@
+import re
+inputFixed = re.sub(r'[^a-zA-Z0-9]', '', input())
+m = re.search( r"(\w)\1", inputFixed)
+if m == None:
+    print(-1)
+else:
+    print(m.group()[0])
+
+
+# Task:
+# You are given a string .
+# Your task is to find the first occurrence of an alphanumeric character in  (read from left to right) that has consecutive repetitions.
+# If no repetitions output -1
+
+# Sample Input:
+# ..12345678910111213141516171820212223
+
+# Sample Output:
+# 1
+
+
+
+
+
+
+
