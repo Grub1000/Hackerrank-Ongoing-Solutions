d0309fcf913be6aff879defc17779893492dadd9
Jorge Devops <bk11iscool@gmail.com>
2025-06-07 13:04:29 -0700

    Solution to the re-start-re-end problem

---
+++ re-start-re-rend-solution.py
@@ -0,0 +1,37 @@
+import re
+S = input()
+k = input()
+regex = r'(?=(' + k + '))'
+m = re.finditer(regex, S)
+# print(m)
+
+count = 0
+for i in m:
+    print( "(" + str(i.start()) + ", " +  str(i.start() + (len(k) - 1)) + ")" )
+    count += 1
+if count == 0:
+    print("(-1, -1)")
+
+
+
+# Task:
+# You are given a string .
+# Your task is to find the indices of the start and end of string  in .
+# 
+# Print the tuple in this format: (start _index, end _index).
+# If no match is found, print (-1, -1).
+
+# Sample Input:
+# aaadaa
+# aa
+# 
+# Sample Output:
+# (0, 1)
+# (1, 2)
+# (4, 5)
+# 
+
+
+
+
+
