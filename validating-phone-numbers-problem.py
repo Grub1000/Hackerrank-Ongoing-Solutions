e0da172f3285bf5fa3cacda881a087e2374dbb85
Jorge Devops <bk11iscool@gmail.com>
2025-06-10 13:33:05 -0700

    Solution to validating-phone-numbers problem

---
+++ validating-phone-numbers-problem.py
@@ -0,0 +1,47 @@
+import re
+
+n = int(input())
+for i in range(n):
+    phoneNumber = input()
+    if re.match(r'[789]\d{9}$', phoneNumber):
+        print("YES")
+    else:
+        print("NO")
+
+
+
+
+
+# Input Format
+
+# The first line contains an integer N, the number of inputs.
+# N lines follow, each containing some string.
+
+
+# Sample Input:
+# 2
+# 9587456281
+# 1252478965
+
+# Sample Output:
+# YES
+# NO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
