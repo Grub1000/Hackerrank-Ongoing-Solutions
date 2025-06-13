ab903fb34c7f96d7dd02c4bd770c37c3764b65de
Jorge Devops <bk11iscool@gmail.com>
2025-06-12 22:19:57 -0700

    Solution to html-parser-part-1 problem

---
+++ html-parser-part-1-problem.py
@@ -0,0 +1,46 @@
+from html.parser import HTMLParser
+
+n = int(input())
+class MyHTMLParser(HTMLParser):
+    
+    def handle_starttag(self, tag, attributes):
+        print('Start :', tag)
+        for element in attributes:
+            print('->', element[0], '>', element[1])
+            
+    def handle_endtag(self, tag):
+        print('End   :', tag)
+        
+    def handle_startendtag(self, tag, attributes):
+        print('Empty :', tag)
+        for element in attributes:
+            print('->', element[0], '>', element[1])
+            
+Parser = MyHTMLParser()
+Parser.feed(''.join([input().strip() for i in range(0, n)]))
+
+
+
+# Sample Input:
+# 2
+# <html><head><title>HTML Parser - I</title></head>
+# <body data-modal-target class='1'><h1>HackerRank</h1><br /></body></html>
+
+# Sample Output:
+# Start : html
+# Start : head
+# Start : title
+# End   : title
+# End   : head
+# Start : body
+# -> data-modal-target > None
+# -> class > 1
+# Start : h1
+# End   : h1
+# Empty : br
+# End   : body
+# End   : html
+
+
+
+
